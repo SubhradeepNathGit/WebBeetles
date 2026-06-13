a64a926383d1e7fb9e0e0b3f700d473b3d7177a2
Subhradeep Nath <subhradeepnathprofessional@gmail.com>
2026-06-13 21:22:01 +0530

    Update getSpecificCourseReview.js

--- src/function/getSpecificCourseReview.js
+++ src/function/getSpecificCourseReview.js
@@ -1,11 +1,11 @@
-import supabase from "../util/supabase/supabase";
+import supabaseAdmin from "../util/supabase/supabaseAdmin";
 
 export const fetchCourseReviews = async (course_id) => {
     // console.log('Receive data for fetching course review', course_id);
 
     if (!course_id) throw new Error("course_id is required");
 
-    const res = await supabase.from("course_reviews").select("*").eq("course_id", course_id)
+    const res = await supabaseAdmin.from("course_reviews").select("*").eq("course_id", course_id)
         .order("created_at", { ascending: false });
     // console.log('Response for fetching course wise review', res);
 
