379d5cef8b35c28438315530ca677d25a39c5c1b
Subhradeep Nath <subhradeepnathprofessional@gmail.com>
2026-06-13 21:06:03 +0530

    Update getTotalRevenue.js

--- src/function/getTotalRevenue.js
+++ src/function/getTotalRevenue.js
@@ -1,8 +1,8 @@
-import supabase from "../util/supabase/supabase";
+import supabaseAdmin from "../util/supabase/supabaseAdmin";
 
 export const getTotalRevenue = async () => {
 
-    const res = await supabase.from('purchases').select('amount');
+    const res = await supabaseAdmin.from('purchases').select('amount');
     // console.log('Response for fetching all amount', res);
 
     return res?.data;
